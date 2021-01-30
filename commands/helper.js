fe7d60c0b8012ddc4ac871237cdc9b6375a50258
Fidel GB <fidelgarcia.soft@gmail.com>
2021-01-29 22:39:01 -0600

    Se cambia la funcion de validar la url de yt a una funcion para obtener los videos de yt en un json

--- commands/helper.js
+++ commands/helper.js
@@ -1,13 +1,46 @@
+const yts = require('yt-search');
 /**
  * 
  * @param {String} url
- * @returns {Boolean} True si es valida la url, false si no lo es 
  */
-const isYtValidUrl = url => {
-    let regex = "^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$";
-    return url.match(regex);
+const getResultsYoutube = async url => {
+    try {
+        let videosRegex = "v=([a-zA-Z0-9\_\-]+)&?";
+        let listRegex = "list=([a-zA-Z0-9\-\_]+)&?"
+        if(url.match(videosRegex)){
+            let videoId = url.match(videosRegex)[0].substring(2);
+            
+            let result = {
+                all:[]
+            }
+            result.all.push(await yts({ videoId: videoId }));
+            return result;
+        }
+        else if(url.match(listRegex)){
+            let listaId = url.match(listRegex)[0].substring(5);
+            
+            let result = await yts({ listId: listaId });
+            return {
+                all: result.videos
+            };
+        }
+        else
+        {
+            let result = await yts(url);
+            return {
+                all: [
+                    result['all'][0]
+                ]
+            }
+        }
+    } catch (error) {
+        return {
+            error: error
+        }
+    }
 }
 
+
 module.exports = {
-    isYtValidUrl: isYtValidUrl
+    getResultsYoutube: getResultsYoutube
 }
