46c50e5daec39de5afeeca486ca9e6793368fde7
Fidel GB <fidelgarcia.soft@gmail.com>
2021-01-29 22:39:25 -0600

    Añadido el comando play

--- commands/music/index.js
+++ commands/music/index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const { join } = require('./join.js');
 const { leave } = require('./leave.js');
+const { play } = require('./play.js');
+const { deleteQueue } = require('./play.js');
 
 /**
  * @param {Array} params - Parametros del comando
@@ -11,7 +13,8 @@ const { leave } = require('./leave.js');
     params = params.slice(1);
     switch(command){
         case 'join': this.connection = await join(message, this.connection); break;
-        case 'leave': this.connection = leave(message, this.connection); break;
+        case 'leave': this.connection = leave(message, this.connection); deleteQueue(); break;
+        case 'play': this.connection = await play(params, message, this.connection); break;
     }
 };
 
